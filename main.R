c72b3d464031adf170e5a8ef61a92b296115ed49
agouy <contact@agouy.com>
2020-06-18 10:51:10 +0000

    first commit

--- main.R
+++ main.R
@@ -1,9 +1,35 @@
 library(tercen)
 library(dplyr)
+library(flowCore)
+library(FlowSOM)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+get_FlowSOM_Clusters <- function(data) {
+  
+  colnames(data) <- ctx$rselect()[[1]]
+  
+  flow.dat <- flowCore::flowFrame(as.matrix(data))
+  
+  fsom <- FlowSOM(
+    dat,
+    colsToUse = 1:ncol(flow.dat),
+    nClus = as.integer(ctx$op.value('nclust')),
+    seed = 1
+  )
+  
+  cluster <- data.frame(
+    cluster = as.character(fsom[[2]][fsom[[1]]$map$mapping[, 1]])
+  )
+  
+  return(cluster)
+}
+
+ctx <- tercenCtx()
+
+ctx %>% 
+  as.matrix() %>%
+  t() %>%
+  get_FlowSOM_Clusters() %>%
+  as_tibble() %>%
+  mutate(.ci = seq_len(nrow(.)) - 1) %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,15 +1,34 @@
 library(tercen)
 library(dplyr)
+library(flowCore)
+library(FlowSOM)
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+options("tercen.workflowId" = "7eee20aa9d6cc4eb9d7f2cc2430313b6")
+options("tercen.stepId"     = "d9da8ab3-ea4f-4da6-88d2-2d8e566040fe")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(median = median(.y)) %>%
+get_FlowSOM_Clusters <- function(data) {
+  colnames(data) <- ctx$rselect()[[1]]
+  dat <- flowCore::flowFrame(as.matrix(data))
+  fsom <- FlowSOM(
+    dat,
+    colsToUse = 1:ncol(dat),
+    nClus = 20,
+    seed = 1
+  )
+  cluster <- data.frame(cluster=as.character(fsom[[2]][fsom[[1]]$map$mapping[, 1]]))
+  return(cluster)
+}
+
+ctx <- tercenCtx()
+
+ctx %>% 
+  as.matrix() %>%
+  t() %>%
+  get_FlowSOM_Clusters() %>%
+  as_tibble() %>%
+  mutate(.ci = seq_len(nrow(.))-1) %>%
   ctx$addNamespace() %>%
   ctx$save()
