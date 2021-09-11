31b1b8069ff08189a031c7d63359ec6553f37000
agouy <contact@agouy.com>
2021-09-11 10:25:37 +0000

    bug fix

--- workspace.R
+++ workspace.R
@@ -2,6 +2,8 @@ library(tercen)
 library(dplyr)
 library(flowCore)
 library(FlowSOM)
+# remotes::install_github("tercen/tim")
+library(tim)
 
 options("tercen.workflowId" = "e9482bf86b951d32f9ff44a499015119")
 options("tercen.stepId"     = "ab7dbc25-6647-4481-84e1-c169bcd24a3b")
@@ -50,81 +52,6 @@ get_FlowSOM_Clusters <- function(data, ctx) {
     cluster_id = as.character(fsom$metaclustering)
   )
   return(list(df_out, fsom))
-}
-
-# here the key for the join start by "." (.model) so it wont be displayed to the user
-serialize_to_string <- function(object) {
-  con <- rawConnection(raw(0), "r+")
-  saveRDS(object, con)
-  str64 <- base64enc::base64encode(rawConnectionValue(con))
-  close(con)
-  return(str64)
-}
-serialise_to_string <- serialize_to_string
-
-deserialize_from_string <- function(str64) {
-  con <- rawConnection(base64enc::base64decode(str64), "r+")
-  object <- readRDS(con)
-  close(con)
-  return(object)
-}
-deserialise_from_string <- deserialize_from_string
-
-get_serialized_result <- function(df, object, object_name, ctx) {
-  
-  df$.object <- object_name
-  
-  # explicitly create relation with .ci and input data
-  columnTable <- ctx$cselect() %>%
-    mutate(.ci = 0:(nrow(.) - 1))
-  
-  leftTable <- data.frame(df) %>%
-    ctx$addNamespace() %>%
-    left_join(columnTable, by = ".ci") %>%
-    select(-.ci) %>%
-    tercen::dataframe.as.table()
-  leftTable$properties$name = 'left'
-
-  leftRelation <- SimpleRelation$new()
-  leftRelation$id <- leftTable$properties$name
-  
-  # the factor where the binary data base64 encoded is stored MUST start by a "." character so it wont be displayed to the user
-  # the factor used in the join relation MUST have a different name then the one used in the leftTable 
-  rightTable <- data.frame(
-    model = object_name,
-    .base64.serialized.r.model = c(serialize.to.string(object))
-  ) %>%
-    ctx$addNamespace() %>%
-    tercen::dataframe.as.table()
-  
-  rightTable$properties$name <- 'right'
-  rightRelation <- SimpleRelation$new()
-  rightRelation$id <- rightTable$properties$name
-  
-  pair <- ColumnPair$new()
-  pair$lColumns <- list(".object") # column name of the leftTable to use for the join
-  pair$rColumns = list(rightTable$columns[[1]]$name) # column name of the rightTable to use for the join (note : namespace has been added)
-  # pair
-
-  join.model = JoinOperator$new()
-  join.model$rightRelation = rightRelation
-  join.model$leftPair = pair
-
-  # create the join relationship using a composite relation (think at a start schema)
-  compositeRelation = CompositeRelation$new()
-  compositeRelation$id = "compositeRelation"
-  compositeRelation$mainRelation = leftRelation
-  compositeRelation$joinOperators = list(join.model)
-
-  # finally return a JoinOperator to tercen with the composite relation
-  join = JoinOperator$new()
-  join$rightRelation = compositeRelation
-
-  result = OperatorResult$new()
-  result$tables = list(leftTable, rightTable)
-  result$joinOperators = list(join)
-
-  return(result)
 }
 
 ctx <- tercenCtx()
